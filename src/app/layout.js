4b4144fe92939fb57cbc4affa887609469591969
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-12-01 16:24:41 +0600

    Admin created suceessfully

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Providers/Provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <Providers>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastContainer />
+          {children}
+        </body>
       </html>
     </Providers>
   );
